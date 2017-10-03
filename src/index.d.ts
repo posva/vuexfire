9efd60e484cff18fca1e813997f79525bc6d8257
Bryan Massoth <btmassoth@gmail.com>
2017-10-03 01:50:59 -0400

    Added JSDoc comments. Updated bindFirebaseRef to take Query as an argument.

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,13 +3,35 @@ import * as firebase from 'firebase';
 export declare const firebaseMutations: Record<string, any>;
 export declare namespace VuexFire {
     interface BindOptions {
+        /**
+         * Cancel callback passed to Firebase when listening for events
+         */
         cancelCallback?: Function;
+
+        /**
+         * Callback called once the data has been loaded. Useful for SSR
+         */
         readyCallback?: ((a: firebase.database.DataSnapshot, b?: string | undefined) => any);
-        errorCallback?: Function;
+
+        /**
+         * Callback called when there is an error loading the data. Useful for SSR
+         */
+        errorCallback?: (error: firebase.FirebaseError) => void;
+
+        /**
+         * (Arrays only) Should Vuexfire wait for the whole array to be populated. Defaults to true
+         */
         wait?: boolean;
     }
     interface ActionContext<S, R> extends Vuex.ActionContext<S, R> {
-        bindFirebaseRef: (key: string, source: firebase.database.Reference, options?: BindOptions) => void;
+        /**
+         * Binds a firebase reference to a property in the state. If there was already another reference bound to the same property, it unbinds it first.
+         */
+        bindFirebaseRef: (key: string, source: firebase.database.Reference | firebase.database.Query, options?: BindOptions) => void;
+
+        /**
+         * Unbinds a bound firebase reference to a given property in the state.
+         */
         unbindFirebaseRef: (key: string) => void;
     }
 }
